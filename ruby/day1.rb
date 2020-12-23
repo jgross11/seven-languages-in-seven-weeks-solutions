d920e1ced75617c0921c7d77098d5d12cfd66df5
jgross11 <jgross11@ycp.edu>
2020-12-23 13:01:56 -0500

    Create day1.rb

---
+++ ruby/day1.rb
@@ -0,0 +1,18 @@
+# Print the string "Hello, world."
+puts 'Hello, world.'
+
+# For the string "Hello, Ruby," find the index of the string "Ruby."
+puts "The index of 'Ruby' in 'Hello, Ruby' is #{'Hello, Ruby'.index('Ruby')}"
+
+# Print your name ten times.
+for i in 1..10
+    puts 'Josh G'
+end
+
+# Print the string "This is a sentence number 1," where the number 1 changes from 1 to 10.
+for i in 1..10
+    puts "This is a sentence number #{i}"
+end
+
+# Run a Ruby program from a file.
+puts "command to run this program: ruby day1.rb"
