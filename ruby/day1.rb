a66cc18576bd6c56df8b0f94910e0fafc84a78df
jgross11 <jgross11@ycp.edu>
2020-12-23 13:13:07 -0500

    Update day1.rb
    bonus problem

--- ruby/day1.rb
+++ ruby/day1.rb
@@ -16,3 +16,29 @@ end
 
 # Run a Ruby program from a file.
 puts "command to run this program: ruby day1.rb"
+
+# Bonus: Write a program that picks a random number. Let a player guess the number, telling the player whether the guess is too low or too high.
+
+# generated random number
+num = rand(10)
+
+# get guess
+puts "Enter guess"
+guess = gets().to_i
+
+# repeat until guess is correct
+until guess == num do
+
+    # guess is too high
+    puts "Guess is too high" if guess > num
+    
+    # guess is too low
+    puts "Guess is too low" if guess < num
+    
+    # get next guess
+    puts "Enter next guess"
+    guess = gets().to_i
+end
+
+# correct guess achieved
+puts "Correct!"
